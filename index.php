ac346c5c962a6925a795123dff5ea2edb271159c
Lawrence <lawrencehome21@gmail.com>
2023-07-07 07:39:13 -0700

    added a modal

--- index.php
+++ index.php
@@ -12,6 +12,23 @@
 <body>
     <h1 class="bg-dark text-light text-center py-2">PHP advanced CRUD</h1>
     <div class="container">
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-10">
                 <div class="input-group">
