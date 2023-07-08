bca26c9a05f86cfe2a74f36af9ad801176ecc5bc
Lawrence <lawrencehome21@gmail.com>
2023-07-08 12:25:42 -0700

    modified the pagination

--- index.php
+++ index.php
@@ -113,9 +113,9 @@
         </table>
         <!-------- Pagination------->
         <nav aria-label="Page navigation example" id="pagination">
-            <ul class="pagination">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <ul class="pagination justify-content-center">
+                <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item active"><a class="page-link" href="#">1</a></li>
                 <li class="page-item"><a class="page-link" href="#">2</a></li>
                 <li class="page-item"><a class="page-link" href="#">3</a></li>
                 <li class="page-item"><a class="page-link" href="#">Next</a></li>
