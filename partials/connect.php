3176d0d3af1f489ff03ebd394734ba681c278be8
Lawrence <lawrencehome21@gmail.com>
2023-07-09 10:50:31 -0700

    corrected some errors

--- partials/connect.php
+++ partials/connect.php
@@ -13,12 +13,14 @@ class Database
     public function __construct()
     {
         try{
-            $databaseConnect = "mysql:host={$this->dbServer};databaseName={$this->dbName};
+            $databaseConnect = "mysql:host={$this->dbServer};dbName={$this->dbName};
             charset=utf8";
             $options=array(PDO::ATTR_PERSISTENT);
             $this->conn = new PDO($databaseConnect, $this->dbUser, $this->dbPassword, $options);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            echo "There was a connection error".$e->getMessage();
         }
-
     }
 }
 ?>
