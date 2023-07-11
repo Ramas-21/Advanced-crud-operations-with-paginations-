e0cb9c42b2277d6666badffa329ccd33a739aa23
Lawrence <lawrencehome21@gmail.com>
2023-07-11 09:05:10 -0700

    added some SQL queries

--- partials/user.php
+++ partials/user.php
@@ -1,12 +1,21 @@
 <?php
 require_once 'connect.php';
 
-class User extends Database{
+class User extends Database {
     protected $tableName="userTable";
 
     // function to add users
     public function add($data){
-        $sql="INSERT INTO {$this->tableName} (pname,email,phone) VALUES(:pname,:email,:phone)";
+        if(!empty($data)){
+            $fields=$placeholder=[];
+            foreach($data as $field =>$value){
+                $fields[]=$field;
+                $placeholder[]=":{field}";
+            }
+        }
+        // $sql="INSERT INTO {$this->tableName} (pname,email,phone) VALUES(:pname,:email,:phone)";
+        $sql="INSERT INTO {$this->tableName} (". implode(',', $fields).") VALUES (". implode(',',$placeholder).")";
     }
 }
+
 ?>
