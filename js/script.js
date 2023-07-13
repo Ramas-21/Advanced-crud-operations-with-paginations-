ba38390b72c8722dbca1b4f03c0d2e97f035cd07
Lawrence <lawrencehome21@gmail.com>
2023-07-13 10:27:58 -0700

    created an ajax code to add users

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@ $(document).ready(function(){
         // Ajax
         $.ajax({
             url:"/advanced-crud-operations/ajax.php",
+            type:"post",
+            dataType:"json",
+            data: new FormData(this),
+            processData:false,
+            contentType:false,
+            beforeSend:function(){
+                console.log("Wait... Data is loading");
+            },
+            success:function(response){
+                console.log(response);
+            },
+            error:function(request,error){
+                console.log(arguments);
+                console.log("Error"+ error);
+            }
+
         })
     })
 })
