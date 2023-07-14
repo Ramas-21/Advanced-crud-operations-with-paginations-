05323ef6e0252b9f4d674967c4e1d6c896ee9926
Lawrence <lawrencehome21@gmail.com>
2023-07-14 10:01:51 -0700

    created an add user action

--- ajax.php
+++ ajax.php
@@ -9,5 +9,15 @@ if(!empty($action)){
     require_once './partials/user.php';
     $obj= new User();
 }
+// adding user action
+if($action=='addUser' && !empty($_POST)){
+    $paName = $_POST['username'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $photo = $_POST['photo'];
+
+    $playerId = (!empty($_POST['userId']))? $_POST['userId']: "";
+    
+}
 
 ?>
